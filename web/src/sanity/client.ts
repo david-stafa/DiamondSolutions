479170844db6035493a9f1df2646691f5c6ecb6f
David Štafa <david.stafa@gmail.com>
2024-07-28 22:09:57 +0200

    sanity imageUrl updated

--- web/src/sanity/client.ts
+++ web/src/sanity/client.ts
@@ -1,27 +1,12 @@
-import { createClient, type QueryParams } from "next-sanity";
+import { createClient } from "next-sanity";
 
+const projectId = "5qdzzsyv";
+const dataset = "production";
+const apiVersion = "2024-01-01";
 
 export const client = createClient({
-  projectId: "5qdzzsyv",
-  dataset: "production",
-  apiVersion: "2024-01-01",
-  // unless you have caching for your front end, `useCdn` should be `true` for most production environments
-  useCdn: false,
+  projectId,
+  dataset,
+  apiVersion, // https://www.sanity.io/docs/api-versioning
+  useCdn: true, // if you're using ISR or only static generation at build time then you can set this to `false` to guarantee no stale content
 });
-
-export async function sanityFetch<QueryResponse>({
-  query,
-  params = {},
-  tags,
-}: {
-  query: string;
-  params?: QueryParams;
-  tags?: string[];
-}) {
-  return client.fetch<QueryResponse>(query, params, {
-    next: {
-      revalidate: process.env.NODE_ENV === "development" ? 30 : 3600,
-      tags,
-    },
-  });
-}
