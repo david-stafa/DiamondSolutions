f4b34fad7d39b2796124dddb0891828738ee99e1
David Štafa <david.stafa@gmail.com>
2024-08-03 13:49:33 +0200

    Validation and required rules added

--- sanity/schemaTypes/content/products.ts
+++ sanity/schemaTypes/content/products.ts
@@ -1,4 +1,28 @@
-import {defineField, defineType} from 'sanity'
+import {defineField, defineType, ValidationContext} from 'sanity'
+
+interface ProductDocument {
+  name: string
+  slug: {current: string}
+  price: number
+  shortDescription?: string
+  longDescription?: string
+  category?: {_ref: string}
+  subcategory?: {_ref: string}
+  image?: {asset: {_ref: string}}
+  productImages?: {asset: {_ref: string}}[]
+}
+
+const validateSubcategory = (value: any, context: ValidationContext) => {
+  const parent = context.parent as ProductDocument;
+  // Ensure `parent` and `parent.category` are defined
+  if (parent?.category?._ref === '56679be4-42b1-49c2-84d3-dd84d398d2a7') {
+    // Perform validation when the category matches the specific ID
+    if (!value) {
+      return 'Subcategory is required when category is Sperky.'
+    }
+  }
+  return true // Return true if validation passes or if no validation needed
+}
 
 export const products = defineType({
   name: 'products',
@@ -32,10 +56,23 @@ export const products = defineType({
       name: 'subcategory',
       type: 'reference',
       to: [{type: 'subcategory'}],
+      hidden: ({parent}) => parent?.category?._ref !== '56679be4-42b1-49c2-84d3-dd84d398d2a7',
+      validation: (Rule) => Rule.custom((value, context) => validateSubcategory(value, context)),
     }),
     defineField({
       name: 'image',
       type: 'image',
+      title: 'Main Image',
+    }),
+    defineField({
+      name: 'productImages',
+      type: 'array',
+      title: 'Product Images',
+      of: [
+        {
+          type: 'image',
+        },
+      ],
     }),
   ],
 })
